d62507a033db8f6dd69a2befa6707689f5abfcc1
Melman520 <113880104+Melman520@users.noreply.github.com>
2022-11-16 23:44:35 -0500

    Agregar archivos de proyecto.

---
+++ ParcialFinal.sql
@@ -0,0 +1,4 @@
+create database ParcialFinal
+go
+use ParcialFinal
+go
---
+++ ParcialFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ParcialFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
